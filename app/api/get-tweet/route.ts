0f1b29480d35e1cc27077645668966b29064f8fc
imaxisXD <sunny735084@gmail.com>
2024-02-02 11:42:08 +0530

    fix: get tweet

--- app/api/get-tweet/route.ts
+++ app/api/get-tweet/route.ts
@@ -1,37 +1,37 @@
-import { api } from "@/convex/_generated/api";
+// import { api } from "@/convex/_generated/api";
 
-import { fetchQuery } from "convex/nextjs";
-import { cookies } from "next/headers";
-import Client from "twitter-api-sdk";
-import { TwitterApi } from "twitter-api-v2";
+// import { fetchQuery } from "convex/nextjs";
+// import { cookies } from "next/headers";
+// import Client from "twitter-api-sdk";
+// import { TwitterApi } from "twitter-api-v2";
 
-export async function GET(request: Request) {
-  const cookieStore = cookies();
+// export async function GET(request: Request) {
+//   const cookieStore = cookies();
 
-  const userData = await fetchQuery(api.users.getUserDetails, {
-    userEmail: user?.email!,
-  });
-  const client = new TwitterApi(userData?.twitter_access_token!);
-  const client2 = new Client(userData?.twitter_access_token!);
-  //   const client2 = client.readWrite;
-  const tweetsOfJack1 = await client2.tweets.usersIdTimeline(
-    userData?.twitter_userID!
-  );
-  console.log(tweetsOfJack1);
+//   const userData = await fetchQuery(api.users.getUserDetails, {
+//     userEmail: user?.email!,
+//   });
+//   const client = new TwitterApi(userData?.twitter_access_token!);
+//   const client2 = new Client(userData?.twitter_access_token!);
+//   //   const client2 = client.readWrite;
+//   const tweetsOfJack1 = await client2.tweets.usersIdTimeline(
+//     userData?.twitter_userID!
+//   );
+//   console.log(tweetsOfJack1);
 
-  //   const info = await client2.currentUserV2();
-  //   console.log(info);
+//   //   const info = await client2.currentUserV2();
+//   //   console.log(info);
 
-  //   const tweetsOfJack2 = await client2.v2.userLikedTweets(
-  //     userData?.twitter_access_token!
-  //   );
+//   //   const tweetsOfJack2 = await client2.v2.userLikedTweets(
+//   //     userData?.twitter_access_token!
+//   //   );
 
-  console.log("Tweet--->", tweetsOfJack1);
-  //   console.log("Tweet--->", tweetsOfJack2);
+//   console.log("Tweet--->", tweetsOfJack1);
+//   //   console.log("Tweet--->", tweetsOfJack2);
 
-  Response.json({
-    t1: tweetsOfJack1,
-    // info: info,
-    // t2: tweetsOfJack2,
-  });
-}
+//   Response.json({
+//     t1: tweetsOfJack1,
+//     // info: info,
+//     // t2: tweetsOfJack2,
+//   });
+// }

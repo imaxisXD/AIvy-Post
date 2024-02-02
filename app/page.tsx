676da2a7c33d1aa7c53dde1af28218248c49cdc8
imaxisXD <sunny735084@gmail.com>
2024-02-02 12:06:37 +0530

    hoja bhai

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { cookies } from "next/headers";
 import Link from "next/link";
 
 export default async function Home() {
-  const cookieStore = cookies();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link

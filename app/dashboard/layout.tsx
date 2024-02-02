144e5a3a2b3480514f481a19592217ac376e7f03
imaxisXD <sunny735084@gmail.com>
2024-02-02 12:03:22 +0530

    fix: suspens

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,5 @@
 import { ReactNode, Suspense } from "react";
-
 import Nav from "@/components/nav";
-
 import Feedbackbar from "@/components/feedbackbar";
 import StoreUserProvider from "@/utils/StoreUserProvider";
 
@@ -12,11 +10,13 @@ export default async function DashboardLayout({
 }) {
   return (
     <StoreUserProvider>
-      <Nav>
-        <Suspense fallback={<div>Loading...</div>}>
-          <h1>ss</h1>
-        </Suspense>
-      </Nav>
+      <Suspense fallback={<h1>Loading</h1>}>
+        <Nav>
+          <Suspense fallback={<div>Loading...</div>}>
+            <h1>ss</h1>
+          </Suspense>
+        </Nav>
+      </Suspense>
       <div className="min-h-screen bg-white-project sm:pl-60 text-black">
         <Feedbackbar />
         {children}
